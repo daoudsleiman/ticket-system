db4362bf1443faff2a73fd70739578e5e0fdbfa6
daoud58 <daoud_sleiman@hotmail.com>
2019-08-17 13:52:34 -0400

    added todos file

---
+++ todos.py
@@ -0,0 +1,10 @@
+
+# Priority is top to bottom
+
+# TODO add staff_member_required to views that need it
+# TODO redesign submission and viewing tasks for users
+# TODO review wording on homepage
+# TODO homepage should display submitted tasks dashboard when non-admin is logged in
+# TODO add image attachments to submit a ticket
+# TODO rename all todo to ticket
+# TODO redesign employee view
